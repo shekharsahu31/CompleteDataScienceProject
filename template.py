d4ecf2abfcba2f8411d176e7e112cc6d86da7822
shekharsahu31 <shekharthe07@gmail.com>
2023-11-20 14:23:37 +0530

    adding template.py - project structure

---
+++ template.py
@@ -0,0 +1,45 @@
+import os # It helps to give path of generic folder
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO) #It reports/ logs basic information regarding running this file with curretn default date and time format.
+
+project_name = "CompleteDataScienceProject"
+
+files = [
+    ## ".github/workflows/.gitkeep"    # made to put main.yaml file for github actions for CI CD pipelines
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_tranier.py",
+    f"src/{project_name}/components/model_monitoring.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+### JUST A NORMAL FUNCTION TO CREATE FOLDER AND FILES.
+for filepath in files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    
+    else:
+        logging.info(f"{filename} is already exists")
